80133357c5d1cada37fce20bc42e7779995f9185
Derya Berkin Gürel <berkingurel@gmail.com>
2023-05-15 14:36:30 +0300

    clean-blog project hw8

--- app.js
+++ app.js
@@ -1,9 +1,23 @@
 const express = require('express');
+const ejs = require('ejs');
 const app = express();
 
+//Statik Dosyaları Kullanmak
+app.use(express.static('public'));
+//Template Engine
+app.set('view engine', 'ejs');
+
 app.get('/', (req, res) => {
-  const blog = { id: 1, title: 'Blog title', description: 'Blog description' };
-  res.send(blog);
+  res.render('index');
+});
+app.get('/about', (req, res) => {
+  res.render('about');
+});
+app.get('/add', (req, res) => {
+  res.render('add_post');
+});
+app.get('/post', (req, res) => {
+  res.render('post');
 });
 
 const port = 3000;
